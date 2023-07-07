a18954cd975472db6720ac732bc289604b5d3e24
F3lip3Dev <35710064+Felipe0307@users.noreply.github.com>
2023-07-06 22:11:14 -0300

    Add files via upload

---
+++ formulário.js
@@ -0,0 +1,38 @@
+function calcularIdade(data) {
+  Year = new Date().getFullyear();
+  return 2023 - 1997;
+}
+
+var nome = "Felipe Rodrigues da Silva";
+var campoDeEstudos = "Rubi, React Native, Php, Sql";
+var idade = 25;
+
+if (nome === "Felipe Rodrigues da Silva") {
+} else if (nome === "David Smith") {
+} else if (nome === "Richard Franklin") {
+} else if (nome === "John Uriat da Silva") {
+} else if (nome === "Tony Azevedo Alves") {
+} else if (nome === "Michael Pereira da Silva") {
+}
+
+if (idade == 25) {
+} else if (idade == 18) {
+} else if (idade == 26) {
+} else if (idade == 28) {
+} else if (idade == 37) {
+} else if (idade == 45) {
+}
+
+function salvaRecruta() {
+  var sobrenome = prompt("last name");
+  campoDeEstudos = prompt("diga outras tecnologia que tem estudado?");
+  var birthYear = parseInt(prompt("1997"));
+  idade = calcularIdade();
+  var textarea = prompt("tell us abou you?");
+  textarea = prompt("tell us about you?");
+}
+
+console.log("Felipe " + ":" + "Rodrigues Da Silva");
+console.log("campo de estudo:" + campoDeEstudos);
+console.log("Idade:" + idade);
+salvaRecruta();
